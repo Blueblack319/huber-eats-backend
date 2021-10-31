765da5673449101da44707621523f76561d2c67f
Hoon <didwogns789@yonsei.ac.kr>
2021-10-31 08:16:15 +0000

    7.1 : set up userService using mockRepository and mockService.

--- src/users/user.service.spec.ts
+++ src/users/user.service.spec.ts
@@ -1,12 +1,50 @@
 import { Test } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { JwtService } from 'src/jwt/jwt.service';
+import { MailService } from 'src/mail/mail.service';
+import { User } from './entities/user.entity';
+import { Verification } from './entities/verification.entity';
 import { UserService } from './users.service';
 
 describe('UserService', () => {
   let service: UserService;
 
+  const mockRepository = {
+    findOne: jest.fn(),
+    save: jest.fn(),
+    delete: jest.fn(),
+    create: jest.fn(),
+  };
+
+  const mockJwtService = {
+    sign: jest.fn(),
+  };
+
+  const mockMailService = {
+    sendVerificationEmail: jest.fn(),
+  };
+
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockRepository,
+        },
+        {
+          provide: getRepositoryToken(Verification),
+          useValue: mockRepository,
+        },
+        {
+          provide: JwtService,
+          useValue: mockJwtService,
+        },
+        {
+          provide: MailService,
+          useValue: mockMailService,
+        },
+      ],
     }).compile();
     service = moduleRef.get<UserService>(UserService);
   });
